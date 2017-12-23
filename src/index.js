c95b60e43899d2b3ef2f94063d4c040bffdb4b29
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-12-23 16:13:08 +0300

    setup database models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import app from './app';
+import models from './models';
 import { port } from './config';
 
 (async () => {
+  try {
+    const { config } = await models.sequelize.sync({});
+    console.log(
+      `Connected to ${config.host}:${config.port}, database: ${config.database}`
+    );
+  } catch (error) {
+    console.error('Unable to connect to database');
+    process.exit(1);
+  }
   await app.listen(port);
   console.log(`Server started on port ${port}`);
 })();
