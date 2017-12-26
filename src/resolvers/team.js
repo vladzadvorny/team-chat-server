75b5af1db1dd056869d5507afd71668be5d832b2
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-12-26 23:25:45 +0300

    add raw query to inviteTeams and start on messages query

--- src/resolvers/team.js
+++ src/resolvers/team.js
@@ -6,7 +6,29 @@ export default {
     allTeams: requiresAuth.createResolver(
       async (parent, args, { models, user }) =>
         models.Team.findAll({ where: { owner: user.id } }, { raw: true })
+    ),
+    inviteTeams: requiresAuth.createResolver(
+      async (parent, args, { models, user }) =>
+        models.sequelize.query(
+          'select * from teams join members on id = team_id where user_id = ?',
+          {
+            replacements: [user.id],
+            model: models.Team
+          }
+        )
     )
+    // inviteTeams: requiresAuth.createResolver(async (parent, args, { models, user }) =>
+    //   models.Team.findAll(
+    //     {
+    //       include: [
+    //         {
+    //           model: models.User,
+    //           where: { id: user.id },
+    //         },
+    //       ],
+    //     },
+    //     { raw: true },
+    //   )),
   },
   Mutation: {
     addTeamMember: requiresAuth.createResolver(
